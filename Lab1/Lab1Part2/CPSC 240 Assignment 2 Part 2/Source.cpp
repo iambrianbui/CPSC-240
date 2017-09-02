cbe572ade966b093a3786fdfc739de779b99bf9a
Joey <joeyliao@outlook.com>
2017-09-02 00:05:16 -0700

    Updates
    Added more code.

--- Lab1/Lab1Part2/CPSC 240 Assignment 2 Part 2/Source.cpp
+++ Lab1/Lab1Part2/CPSC 240 Assignment 2 Part 2/Source.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 void displayShape();
+int calcTriArea(int c, int h);
 int calcTriPeri(int a, int b, int c);
+int calcRectArea(int width, int lengths);
+int calcRectPeri(int width, int lengths);
 
 int main() {
 	int a, b, c, h, triArea, triPeri;
@@ -13,9 +16,16 @@ int main() {
 	cout << "Enter the length and the width of the rectangle: ";
 	cin >> length >> width;
 
-	cout << "\tTriangle\n\t\tArea...............";
-	cout << "\nPerimeter..........";
+	triArea = calcTriArea(c, h);
 	triPeri = calcTriPeri(a, b, c);
+	rectArea = calcRectArea(width, length);
+	rectPeri = calcRectPeri(width, length);
+
+	cout << "\tTriangle\n\t\tArea..............." << triArea;
+	cout << "\n\t\tPerimeter.........." << triPeri << endl;
+
+	cout << "\tRectangle\n\t\tArea..............." << rectArea;
+	cout << "\n\t\tPerimeter.........." << rectPeri << endl;
 
 	return 0;
 }
@@ -27,25 +37,44 @@ void displayShape() {
 	cout << "  /  h|   \\  \t|           |\n";
 	cout << " /    |    \\ \t|           |\n";
 	cout << " -----------  \t-------------\n";
-	cout << "      c       \t   length      ";
+	cout << "      c       \t   length    \n";
+}
+
+int calcTriArea(int c, int h) {
+	int area;
+	_asm {
+		mov eax, c
+		imul h
+		//cannot finish because need to divide by 2. h*base/2
+	}
 }
 
 int calcTriPeri(int a, int b, int c) {
 	int perimeter;
 	_asm {
-		mov eax,a
-		add eax,b
-		add eax,c
+		mov eax, a
+		add eax, b
+		add eax, c
 		mov perimeter, eax
 	}
 	return perimeter;
 }
 
-int calcRectPeri(int width, int length) {
+int calcRectArea(int width, int lengths) {
+	int area;
+	_asm {
+		mov eax, width
+		imul lengths
+		mov area, eax
+	}
+	return area;
+}
+
+int calcRectPeri(int width, int lengths) { //needs to be "lengths", not "length" because assembly language uses "length" already
 	int perimeter;
 	_asm {
 		mov eax, width
-		add eax, length
+		add eax, lengths
 		add eax, eax
 		mov perimeter, eax
 	}
