8109c0999e8e3a6ca08b4295d8bf781783a85def
Joey <joeyliao@outlook.com>
2017-09-06 21:52:38 -0700

    Added completed part 2 and part 3

--- Lab1/Lab1Part2/CPSC 240 Assignment 2 Part 2/Source.cpp
+++ Lab1/Lab1Part2/CPSC 240 Assignment 2 Part 2/Source.cpp
@@ -1,3 +1,13 @@
+/*---------------------------------------------------------------
+CPSC240
+Created by:  Bui, Brian and Liao, Joey
+Created on:  5 Sept 2017
+Last edited:  6 Sept 2017
+This program will prompt for a sides and height of a triangle and 
+width and length of the rectangle.  
+It will then use ASSEMBLY to calculate the conversion.
+-----------------------------------------------------------------*/
+
 #include <iostream>
 using namespace std;
 
@@ -43,10 +53,14 @@ void displayShape() {
 int calcTriArea(int c, int h) {
 	int area;
 	_asm {
-		mov eax, c
-		imul h
-		//cannot finish because need to divide by 2. h*base/2
+		mov eax, c     ; eax = c
+		mov ebx, h     ; ebx = h
+		imul ebx       ; eax = h*c
+		mov ecx, 2     ; ecx = 2
+		idiv ecx       ; h*c / 2
+		mov area, eax
 	}
+	return area;
 }
 
 int calcTriPeri(int a, int b, int c) {
@@ -80,3 +94,21 @@ int calcRectPeri(int width, int lengths) { //needs to be "lengths", not "length"
 	}
 	return perimeter;
 }
+/*
+     /|\        -------------
+    / | \       |           |
+a  /  |  \  b   |           |width
+  /  h|   \     |           |
+ /    |    \    |           |
+ -----------    -------------
+       c            length
+Enter the values of a,b,c and h for the triangle: 4 6 9 10
+Enter the length and the width of the rectangle: 15 10
+    Triangle
+        Area...............45
+        Perimeter..........19
+	Rectangle
+	    Area...............150
+	    Perimeter..........50
+Press any key to continue . . .
+*/
