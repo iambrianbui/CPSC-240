a7aa1109a7668cdeb4c1a21f3d567c722334a422
iambrianbui <iambrianbui@yahoo.com>
2017-09-12 22:32:37 -0700

    Lab3Part1 code done, needs comments

--- Lab3/Lab3Part1/Lab3Part1/Source.cpp
+++ Lab3/Lab3Part1/Lab3Part1/Source.cpp
@@ -4,48 +4,81 @@
 using namespace std;
 
 void menu();
-void prompt();
-int calculate(int, int, int, int);
+void promptDrinks();
+void promptSandwiches();
+void calculate();
+
+int totalBill, amtDrink, amtSand, sandChoice;
+char drinkChoice;
 
 int main() {
 
 	menu();
-	prompt();
+	calculate();
+	system("pause");
 	return 0;
 }
 
 void menu(){
 	string dots = "....................";  //  20 dots
 	cout << "----------7-11 Convenient Store----------" << endl;
-	cout << "Soda(S)" << dots << "$2" << endl;
+	cout << "Drinks" << endl;
+	cout << "\tSoda(S)...." << dots << "$2" << endl;
+	cout << "\tWater(W)..." << dots << "$1" << endl;
+	cout << "Sandwiches" << endl;
+	cout << "\t10 inches.." << dots << "$3" << endl;
+	cout << "\t12 inches.." << dots << "$5" << endl;
 }
 
-void prompt(){
-	int totalBill, amtDrink, drinkChoice, amtSand, sandChoice;
-	char drinkCh;
-
+void promptDrinks(){
 	cout << "How many drinks?  ";
 	cin >> amtDrink;
-	cout << endl << "\tWhat kind of drink?  (S=Soda, W=Water)";
-	cin >> drinkCh;
-	if (drinkCh == 'S' || drinkCh == 's') {
-		drinkChoice = 2;
-	}
-	else drinkChoice = 1;
-	cout << endl << "How many Sandwiches?";
-	cin >> amtSand;
-	cout << endl << "\tWhat size of sandwich (10/12 inches)?";
-	cin >> sandChoice;
-	if (sandChoice == 10) {
-		sandChoice = 3;
-	}
-	else sandChoice = 5;
-
-
-
-	totalBill = calculate(amtDrink, drinkChoice, amtSand, sandChoice);
+	cout << "\tWhat kind of drink?  (S=Soda, W=Water)  ";
+	cin >> drinkChoice;
 }
 
-int calculate(int amtDrink, int drinkChoice, int amtSand, int sandChoice) {
-	return 1;
+void promptSandwiches() {
+	cout << endl << "How many Sandwiches?  ";
+	cin >> amtSand;
+	cout << "\tWhat size of sandwich (10/12 inches)?  ";
+	cin >> sandChoice;
+}
+
+void calculate() {
+	_asm {
+			call	promptDrinks
+			cmp		drinkChoice, 'S' 
+			je		SodaDrink
+
+			cmp		drinkChoice, 's'
+			je		SodaDrink
+
+			mov		EAX, 1
+			imul	amtDrink
+			jmp		DrinkDone
+	
+		SodaDrink:
+			mov		EAX, 2
+			imul	amtDrink 
+
+		DrinkDone:
+			mov		totalBill, EAX
+
+			call	promptSandwiches
+			cmp		sandChoice, 10
+			je		Sand10 
+
+			mov		EAX, 12
+			imul	amtSand
+			jmp		SandDone
+
+		Sand10:
+			mov		EAX, 10
+			imul	amtSand
+
+		SandDone:
+			add		totalBill, EAX
+
+	}
+	cout << "Total bill:  " << totalBill;
 }
