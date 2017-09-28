3d02f2f6432197dce10cba9e12cca54bad27aca6
iambrianbui <iambrianbui@yahoo.com>
2017-09-27 22:47:04 -0700

    S.O.S. joey (displayDefective function)

--- Lab5/Lab5Part3.cpp
+++ Lab5/Lab5Part3.cpp
@@ -2,7 +2,7 @@
 CPSC240
 Created by:  Bui, Brian and Liao, Joey
 Created on:  26 September 2017
-Last edited:  26 September 2017
+Last edited:  27 September 2017
 This program will identify which sprinklers are on and off
 given a hexadecimal number.
 -----------------------------------------------------*/
@@ -10,29 +10,32 @@ given a hexadecimal number.
 #include<iostream>
 using namespace std;
 
-unsigned short moment = 0x6A2F;
-int amountofOn = 1;
+unsigned short moment = 0x6A2F;					//  AX register at this moment
+int amountofOn = 0;
 int defective;
 
 void convertToBinary();
 void checkOnSprink();
 void displayDefective();
+void displayD();
 
 
 int main() {
 
 	convertToBinary();
 	checkOnSprink();
+	displayDefective();
 	
 	system("pause");
 	return 0;
 }
 
+//  convert to binary from hexadec
 void convertToBinary() {
-	unsigned short x = 1 << 15;
-	unsigned short tempMoment = moment;
-	short t;
-	cout << x << endl;
+	unsigned short x = 1 << 15;					//  shift over 15 to the left
+	unsigned short tempMoment = moment;			
+	short t;									//  proxy
+	cout << "AX = ";
 	for (int i = 1; i <= 16; i++) {
 		t = tempMoment & x;
 		tempMoment = tempMoment << 1;
@@ -47,18 +50,19 @@ void convertToBinary() {
 	cout << endl;
 }
 
+//  check which sprinklers are on
 void checkOnSprink() {
 	_asm {
 		mov		AX, 0x6A2F
 		mov		EBX, 0					;  index  
 
 	ForLoop:
-		cmp		EBX, 15
+		cmp		EBX, 16
 		jge		DoneLoop				;  finish loop
 
-		mov		CX, 0xF000
-		and		CX, AX 
-		shr		CX, 15
+		mov		CX, 0xF000				;  set to 1000 ...
+		and		CX, AX					
+		shr		CX, 15					;  see if it's 1 or 0
 		cmp		CX, 1
 		je		IsOn
 
@@ -79,6 +83,37 @@ void checkOnSprink() {
 	cout << amountofOn << " sprinklers are ON. " << endl;
 }
 
+//  display which sprinklers are off
 void displayDefective() {
-	cout << defective;
+	cout << "Defective sprinklers:  ";
+	_asm {
+		mov		AX, 0x6A2F
+		mov		EBX, 16					;  index
+
+	ForLoop2:
+		cmp		EBX, 0
+		jle		DoneLoop2				;  finish loop
+
+		mov		CX, 1
+		and		CX, AX 
+		cmp		CX, 1
+		je		ShiftRight 
+		
+		mov		defective, EBX 
+		call	displayD
+
+	ShiftRight:
+		dec		EBX 
+		shr		AX, 1
+		jmp		ForLoop2
+
+
+
+	DoneLoop2:
+	}
+	cout << endl;
+}
+
+void displayD() {
+	cout << defective << " ";
 }
