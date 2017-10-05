26a7c5e976bbac4630a84c3e1a36374afa84f800
iambrianbui <iambrianbui@yahoo.com>
2017-10-05 00:19:01 -0700

    phew

--- Lab6/Lab6Part2.cpp
+++ Lab6/Lab6Part2.cpp
@@ -2,7 +2,7 @@
 CPSC240
 Created by:  Bui, Brian and Liao, Joey
 Created on:  3 October 2017
-Last edited:  3 October 2017
+Last edited:  5 October 2017
 This program will use assembly to bubble sort.
 
 */
@@ -10,9 +10,12 @@ This program will use assembly to bubble sort.
 #include<iostream>
 
 int a[5] = { 9,3,22,8,1 };
+int index = 0, indexJ = 0;
 
-void bubbleSort();
-void displayArray();
+void bubbleSort();										//  2d forloop
+void checkBubble();										//  do we have to switch?
+void displayArray();									//  display array
+void swapB();											//  swap the elements
 
 using namespace std;
 
@@ -26,38 +29,55 @@ int main() {
 }
 
 void bubbleSort() {
-	int n = 5;
-	int i = 0;
-	int j = 0;
-	int temp;
+	int temp = 0, tempECX, tempEBX;						//  using a lot of temp variables as substitutes for registers
 	_asm {
-		mov				eax, i
-	Loop1:  
-		cmp				eax, 4
-		jge				DoneLoop
+		mov			ebx, index 
+	ForLoop1:
+		cmp			ebx, 4
+		jge			DoneLoop
+		mov			indexJ, 0							;  IMPORTANT TO STOP INFINITE LOOP!!!!
+		mov			ecx, indexJ
 
-			mov				ebx, j
-			Loop2:  
-				mov				ecx, n
-				sub				ecx, eax
-				sub				ecx, 1 
-				cmp				ebx, n
-				jge				OutOfLoop 
 
-					cmp				[a + ebx*4], [a + 4 + ebx*4]
-					jle				Next
-					mov				temp, [a + ebx * 4]
-					mov				[a + ebx * 4], [a + 4 + ebx * 4]
-					mov				[a + 4 + ebx*4], temp
+		ForLoop2:
+			mov			temp, 5
+			sub			temp, ebx
+			dec			temp
+			cmp			ecx, temp
+			jge			Done2
+			mov			tempECX, ecx					;  proxy 
+			mov			tempEBX, ebx					;  proxy
 
-				Next:
+			call		checkBubble						;  see if we need to bubbleSort
+			mov			ecx, tempECX
+			inc			ecx
+			mov			indexJ, ecx
+			jmp			ForLoop2
 
-				inc				ebx
-				
-		OutOfLoop:
-		inc				eax
+
+		Done2:					
+			mov			ebx, tempEBX
+			inc			ebx 
+			jmp			ForLoop1 
 
 	DoneLoop:
+
+
+	}
+
+
+}
+
+void checkBubble() {
+	_asm {
+		mov			eax, indexJ							;  get the index 
+		mov			ebx, [a + eax * 4]					;  a[j]
+		mov			ecx, [a + 4 + eax * 4]				;  a[j+1]
+		cmp			ebx, ecx							;  check if greater than 
+		jle			Done
+		call		swapB
+	Done:  
+
 	}
 }
 
@@ -70,3 +90,10 @@ void displayArray() {
 	cout << endl;
 }
 
+void swapB() {
+	int temp;
+
+	temp = a[indexJ];									//  pass the index through the global variable
+	a[indexJ] = a[indexJ+1];
+	a[indexJ+1] = temp;
+}
